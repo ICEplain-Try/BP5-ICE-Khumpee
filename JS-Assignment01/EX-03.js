f7118354d58ee30f53e9015486f5609ea79895f3
wirida <info.icejour@gmail.com>
2024-08-06 17:56:22 +0700

    Assignment01 : JavaScript Values Variables

---
+++ JS-Assignment01/EX-03.js
@@ -0,0 +1,6 @@
+// Exercise #3: Celsius to Fahrenheit
+let celsius = 30;
+
+const fahrenheit = (celsius * 1.8) + 32;
+
+console.log (`อุณหภูมิ 30 องศาเซลเซียส = ${fahrenheit} องศาฟาเรนไฮต์`);
---
+++ JS-Assignment01/EX-04.js
@@ -0,0 +1,13 @@
+// Exercise #4: What are Expressions
+
+130 + 300; // Exercise
+let zero = 1000 - 1000;
+20 * 120; // Exercise
+1000 / 2; // Exercise
+typeof true; // Exercise
+let name = "John";
+3 > 5; // Exercise
+10 == 100; // Exercise
+200; // Exercise
+
+// วิธีตรวจคือการลองใช้ console.log () แล้วใส่โค้ดทั้งบรรทัดต่างๆลงในวงเล็บ
